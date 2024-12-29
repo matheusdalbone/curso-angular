9c481539649c4765d94e793fbfd3e2e52c0e4499
matheusdalbone <matheusdalbone7@gmail.com>
2024-12-29 19:26:47 -0300

    feat: renderização condicional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ParentDataComponent } from "./components/parent-data/parent-data.component";
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent],
+  imports: [RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
