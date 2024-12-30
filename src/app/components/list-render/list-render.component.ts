89194e077c69b1d3d90dc9d0d9721eb11ad16c28
matheusdalbone <matheusdalbone7@gmail.com>
2024-12-29 21:57:45 -0300

    feat: interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Animal } from '../../interfaces/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -8,30 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './list-render.component.css'
 })
 export class ListRenderComponent {
-  animals = [
+  animals: Animal[] = [
     {
-      name: "turca", type: "dog"
+      name: "turca", type: "dog", age: 2
     },
     {
-      name: "edna", type: "cat"
+      name: "edna", type: "cat", age: 2
     },
     {
-      name: "tom", type: "dog"
+      name: "tom", type: "dog", age: 6
     },
     {
-      name: "frida", type: "dog"
+      name: "frida", type: "dog", age: 5
     },
     {
-      name: "camila", type: "cat"
+      name: "camila", type: "cat", age: 2
     },
     {
-      name: "bob", type: "horse"
+      name: "bob", type: "horse", age: 6
     },
     {
-      name: "pitu", type: "bird"
+      name: "pitu", type: "bird", age: 4
     },
     {
-      name: "tubias", type: "shark"
+      name: "tubias", type: "shark", age: 1
     }
   ]
+
+  animalDetais = "";
+
+  showAge = (animal: Animal) => {
+    this.animalDetais = `O pet ${animal.name} tem ${animal.age} anos`
+  }
 }
