37ff8f11f881abcac07a01f2fa9bc30d274800c4
matheusdalbone <matheusdalbone7@gmail.com>
2024-12-31 14:12:51 -0300

    feat: routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ParentDataComponent } from "./components/parent-data/parent-data.component";
-import { DirectivesComponent } from './components/directives/directives.component';
-import { IfRenderComponent } from './components/if-render/if-render.component';
-import { EventosComponent } from './components/eventos/eventos.component';
-import { EmiterComponent } from './components/emiter/emiter.component';
-import { ListRenderComponent } from './components/list-render/list-render.component';
-import { PipesComponent } from './components/pipes/pipes.component';
-import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { AppRoutes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ParentDataComponent, DirectivesComponent, IfRenderComponent, EventosComponent, EmiterComponent, ListRenderComponent, PipesComponent, TwoWayBindingComponent],
+  imports: [RouterOutlet, AppRoutes],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { NgModule } from '@angular/core';
+import { EmiterComponent } from './components/emiter/emiter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: FirstComponentComponent
+  },
+  { 
+    path: 'list', component: ListRenderComponent
+  },
+  {
+    path: 'number', component: EmiterComponent
+  }
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutes { };
